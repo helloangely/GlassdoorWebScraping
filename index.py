43296a8c6c0ed74aad3a866e04dfddeaf2718a55
Angely Philip <anphilip@indiana.edu>
2016-11-04 23:45:45 -0400

    job progession

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import urllib2
 import json
 import socket
 import os
-
+from pprint import pprint
 
 def get_ip():
 	ip_address = socket.gethostbyname(socket.gethostname())
@@ -21,24 +21,32 @@ def get_enviorn_key_id():
 	return api_id,api_key
 
 def get_data():
+	'''
+	Getting dev job progressionls
+	'''
 
 	ip_address = get_ip()
 
 	api_id,api_key = get_enviorn_key_id()
 
-	action = 'employers'
+	action = 'jobs-prog'
 	query = 'pharmaceuticals'
 	base_url = 'http://api.glassdoor.com/api/api.htm?'
 	useragent = 'Mozilla/%2F4.0'
+	countryId = '1'
+	jobTitle= 'developer'
 
-	url = '{5}v=1&format=json&t.p={0}&t.k={1}&action={3}&q={4}&userip={2}&useragent={6}'.format(api_id,api_key,ip_address,action,query,base_url,useragent)
+
+	url = '{5}v=1&format=json&t.p={0}&t.k={1}&action={3}&q={4}&userip={2}&useragent={6}&countryId={7}&jobTitle={8}'.format(api_id,api_key,ip_address,action,query,base_url,useragent,countryId,jobTitle)
 	hdr = {'User-Agent': 'Mozilla/5.0'}
 
 	req = urllib2.Request(url,headers=hdr)
 	response = urllib2.urlopen(req)
 	json_data = json.load(response) 
 
-	print json_data
+	pprint(json_data)
+	
+
 
 get_data()
 
